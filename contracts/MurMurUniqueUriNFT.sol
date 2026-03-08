15b4790b9bcd490aad5f5e6bbe005ed51c71958d
MrDiddyz <85sq8z8x98@privaterelay.appleid.com>
2026-03-08 08:33:45 +0100

    Strengthen NFT mint contract and add luxury mint UI mock

--- contracts/MurMurUniqueUriNFT.sol
+++ contracts/MurMurUniqueUriNFT.sol
@@ -2,33 +2,41 @@
 pragma solidity ^0.8.20;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MurMurUniqueUriNFT is ERC721URIStorage, Ownable {
+contract MurMurUniqueUriNFT is ERC721URIStorage, Ownable, ReentrancyGuard {
     error InvalidReceiver();
     error EmptyTokenURI();
     error TokenURIAlreadyMinted();
+    error TokenURITooLong(uint256 maxLength, uint256 actualLength);
     error IncorrectMintFee(uint256 expected, uint256 actual);
     error WithdrawalFailed();
 
     event NFTMinted(address indexed to, uint256 indexed tokenId, string tokenURI, uint256 paid);
     event FundsWithdrawn(address indexed to, uint256 amount);
+    event MintFeeUpdated(uint256 previousMintFee, uint256 newMintFee);
 
     mapping(string => bool) public mintedTokenURIs;
 
-    uint256 public immutable mintFee;
+    uint256 public mintFee;
+    uint256 public immutable maxTokenURILength;
     uint256 private _nextTokenId;
 
-    constructor(address owner_, uint256 mintFee_)
+    constructor(address owner_, uint256 mintFee_, uint256 maxTokenURILength_)
         ERC721("MurMur Unique URI NFT", "MMURI")
         Ownable(owner_)
     {
         mintFee = mintFee_;
+        maxTokenURILength = maxTokenURILength_;
     }
 
-    function mintNFT(address to, string calldata tokenURI_) external payable {
+    function mintNFT(address to, string calldata tokenURI_) external payable nonReentrant {
         if (to == address(0)) revert InvalidReceiver();
-        if (bytes(tokenURI_).length == 0) revert EmptyTokenURI();
+
+        uint256 uriLength = bytes(tokenURI_).length;
+        if (uriLength == 0) revert EmptyTokenURI();
+        if (uriLength > maxTokenURILength) revert TokenURITooLong(maxTokenURILength, uriLength);
         if (mintedTokenURIs[tokenURI_]) revert TokenURIAlreadyMinted();
         if (msg.value != mintFee) revert IncorrectMintFee(mintFee, msg.value);
 
@@ -48,7 +56,13 @@ contract MurMurUniqueUriNFT is ERC721URIStorage, Ownable {
         return _nextTokenId;
     }
 
-    function withdraw(address payable to) external onlyOwner {
+    function setMintFee(uint256 newMintFee) external onlyOwner {
+        uint256 previousMintFee = mintFee;
+        mintFee = newMintFee;
+        emit MintFeeUpdated(previousMintFee, newMintFee);
+    }
+
+    function withdraw(address payable to) external onlyOwner nonReentrant {
         if (to == address(0)) revert InvalidReceiver();
 
         uint256 balance = address(this).balance;
